7103f639fcf02bb8d5516c50098e1e74fb93905e
bigboytimi <olutimiolowookere@icloud.com>
2023-05-09 08:50:22 +0100

    Initial commit

---
+++ src/main/java/org/application/lendr/LendrApplication.java
@@ -0,0 +1,13 @@
+package org.application.lendr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LendrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LendrApplication.class, args);
+	}
+
+}
